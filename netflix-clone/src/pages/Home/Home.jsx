fdb9a307fde290207b44c04350096253a1ff9fc7
AlexB <alexb@example.com>
2025-11-28 18:50:22 +0300

    add Header and Footer sections for Netflix clone UI

---
+++ netflix-clone/src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../componentes/Header/header'
+import Footer from '../../componentes/Footer/Footer'
+
+function Home() {
+  return (
+    <div>
+    <Header />
+    <Footer />
+    </div>
+  )
+}
+
+export default Home
